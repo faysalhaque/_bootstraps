d7c20ad98badcc403651ddeed7cfb26934dcd5f8
Faysal haque <faysal.haque@gmail.com>
2014-04-16 16:26:52 +0600

    First commit. Adding a README.

--- footer.php
+++ footer.php
@@ -1,36 +1,162 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the id=main div and all content after
- *
- * @package _bootstraps
- * @package _bootstraps - 2013 1.0
- */
-?>
-</div><!-- .row -->
-</div><!-- .container -->
-</div><!-- #main .site-main -->
+<!-- FOOTER -->
+        <footer>
+            <div class="first-footer-widget-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-9">
+                            <div class="sidebar-widget">
+                                <div class="subscribe_widget">
+                                    <div class="newsletter">
+                                        <h3 class="widget-title heading-title">newsletter sign-up</h3>
+                                        <span>Sed ut perspiciatis unde omnis.</span>
+                                    </div>
+                                    <form action="#" method="post" target="popupwindow">
+                                        <p class="subscribe-email">
+                                            <input type="text" name="email" class="subscribe_email" placeholder="enter your email address">
+                                        </p>
+                                        <button class="button" type="submit" title="Subscribe"><span><span>Subscribe</span></span></button>
+                                        <p style="display:none;">Delivered by <a href="http://www.feedburner.com" target="_blank">FeedBurner</a></p>
+                                    </form>
+                                </div>
+                            </div><!-- end siebar-widget -->
+                        </div>
 
-<footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="site-info">
-                    <?php do_action( 'tp_credits' ); ?>
+                        <div class="col-md-3">
+                            <div class="sidebar-widget">
+                            <div class="social-widget">
+                                <h3>Follow us: </h3>
 
-                    &copy; 2008-<?php echo date( 'Y' ) ?> <a href="http://tareq.wedevs.com">Tareq Hasan</a>. All rights are reserved.
-                    Powered by <a href="http://wordpress.org/" target="_blank" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wedevs' ); ?>" rel="generator"><?php printf( __( '%s', 'wedevs' ), 'WordPress' ); ?></a>.
+                                <ul class="social-links clearfix">
+                                    <li class="facebook"><a href="" title="Become our fan">Facebook</a></li>
+                                    <li class="twitter"><a href="" title="Follow us">Twitter</a></li>
+                                    <li class="rss"><a href="" title="Get updates">RSS</a></li>
+                                </ul>
+                            </div><!-- end social-widget -->
+                            </div>
+                        </div>
+                    </div><!-- end .row -->
+                </div><!-- end .container -->
+            </div><!-- end .first-footer-widget-area -->
 
-                    <?php echo wedevs_get_option( 'footer_text', 'tp_settings' ); ?>
-                </div><!-- .site-info -->
-            </div>
-        </div><!-- .row -->
-    </div><!-- .container -->
-</footer><!-- #colophon .site-footer -->
-</div><!-- #page .hfeed .site -->
+            <div class="second-footer-widget-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="sidebar-widget">
+                                <h3>Latest From Blog</h3>
+                                <ul class="blog-post-widget">
+                                    <li><a href="#">commerce before one-to-one intellectual</a></li>
+                                    <li><a href="#">Quickly maximize inexpensive web services</a></li>
+                                    <li><a href="#">Appropriately e-enable mission-critical metrics </a></li>
+                                    <li><a href="#">Cum sociis natoque penatibus et magnis dis</a></li>
+                                    <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+                                </ul>
+                            </div><!-- end .sidebar-widget -->
+                        </div><!-- end .col-md-3 -->
+                        <div class="col-md-3">
+                           <div class="sidebar-widget">
+                               <h3>Information</h3>
 
-<?php wp_footer(); ?>
+                               <ul>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Our Location</a></li>
+                                    <li><a href="#">Contact Us </a></li>
+                                    <li><a href="#">Support</a></li>
+                                    <li><a href="#">Our Company</a></li>
+                                    <li><a href="#">Our Staff</a></li>
+                                </ul>
+                           </div><!-- end .sidebar-widget -->
+                            
+                        </div><!-- end .col-md-3 -->
+                        <div class="col-md-3">
+                            <div class="sidebar-widget">
+                                <h3>About Store</h3>
 
-</body>
+                                <div class="textwidget">
+                                    <p>Duis at nisl quis quam condimentum pulvinar. Quisque euismod convallis eros, quis lacinia enim rhoncus sed. Nulla facilisi.</p>
+                                    <p style="margin-bottom:2px;">Phone: <strong>(01) 123 456 RC</strong></p>
+                                    <p style="margin-bottom:2px;">Fax: <strong>(01) 123 567 RC</strong></p>
+                                    <p style="margin-bottom:2px;">Email: <a href="#" title="">support@wedevs.com</a></p> 
+                                </div>
+                            </div><!-- end .sidebar-widget -->
+                        </div><!-- end .col-md-3 -->
+                        <div class="col-md-3">
+                            <div class="sidebar-widget">
+                                <h3>Sell On Dokan</h3>
+
+                                <ul>
+                                    <li><a href="#">Open a Shop</a></li>
+                                    <li><a href="#">Seller Handbook</a></li>
+                                </ul>
+                            </div><!-- end .sidebar-widget -->
+                        </div><!-- end .col-md-3 -->
+                    </div><!-- end .row -->
+                </div><!-- end .row -->
+            </div><!-- end .second-footer-widget-area -->
+
+            <div class="third-footer-area">
+                <div class="container">
+                    <div class="row">
+                        <aside class="payment col-md-6">
+                            <div class="payment-options pull-left">
+                                <h3 class="payment_title">payment accept</h3>
+                                <ul class="list-inline">
+                                    <li><a href="#"><img alt="visa" src="assets/images/icon/ic_payment_visa.png"></a></li>
+                                    <li><a href="#"><img alt="master card" src="assets/images/icon/ic_payment_master_card.png"></a></li>
+                                    <li><a href="#"><img alt="express" src="assets/images/icon/ic_payment_american.png"></a></li>
+                                    <li><a href="#"><img alt="paypal" src="assets/images/icon/ic_payment_paypal.png"></a></li>
+                                    <li><a href="#"><img alt="skrill" src="assets/images/icon/ic_payment_skrill.png"></a></li>
+                                </ul>
+                            </div>
+                        </aside>
+                        <aside class="service col-md-6"> 
+                            <div class="shipping-service pull-right">
+                                <h3 class="service-title">shipping service</h3>
+                                <ul class="list-inline">
+                                    <li><a href="#"><img alt="z_service" src="assets/images/icon/ic_service_01.png"></a></li>
+                                    <li><a href="#"><img alt="fedex" src="assets/images/icon/ic_service_02.png"></a></li>
+                                    <li><a href="#"><img alt="ups" src="assets/images/icon/ic_service_03.png"></a></li>
+                                <ul>
+                            </div>
+                        </aside>
+                    </div><!-- end .row -->
+                </div><!-- end .container -->
+            </div><!-- end  .third-footer-area-->
+
+            <!-- FOURTH FOOTER WIDGET AREA -->
+            <div class="fourth-footer-widget-area">
+                <div class="container"> 
+                    <div class="row">
+                        <div class="menu-menu-fourth-footer-container">
+                            <ul class="menu list-inline">
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">Our Location</a></li>
+                                <li><a href="#">Support</a></li>
+                                <li><a href="#">Primary Shortcode</a></li>
+                                <li><a href="#">Site Map Template</a></li>
+                                <li><a href="#">Archive Template</a></li>
+                            </ul>
+                        </div>
+                    </div><!-- end .row -->
+                </div><!-- end .container -->
+            </div><!-- end fourth0footer-widget-area -->
+
+            <!-- FIFTH FOOTER AREA -->
+            <div class="fifth-footer-area">
+                <div class="container">
+                    <div class="row">
+                        <div id="copy-right-container" class="copy-right col-md-12">
+                            <div class="copyright">
+                                &copy; 2014 Dokan Wordpress Store. All Rights Reversed. WordPress Templates by 
+                                <a href="#" title="WordPress Themes">Dokan.me</a>
+                            </div>
+                        </div><!-- end #copyright -->
+                    </div><!-- end .row -->
+                </div><!-- end .container -->
+            </div><!-- end .fifth-footer-area -->
+        </footer>
+        <?php wp_footer(); ?>
+    </body>
 </html>
--- header.php
+++ header.php
@@ -1,100 +1,205 @@
 <?php
+if ( !function_exists( 'WC' ) ) {
+    wp_die( sprintf( __( 'Please install <a href="%s"><strong>WooCommerce</strong></a> plugin first', 'dokan' ), 'http://wordpress.org/plugins/woocommerce/' ) );
+}
 /**
  * The Header for our theme.
  *
  * Displays all of the <head> section and everything up till <div id="main">
  *
- * @package _bootstraps
- * @package _bootstraps - 2013 1.0
+ * @package dokan
+ * @package dokan - 2014 1.0
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php
+        /*
+         * Print the <title> tag based on what is being viewed.
+         */
+        global $page, $paged;
 
-	wp_title( '|', true, 'right' );
+        wp_title( '|', true, 'right' );
 
-	// Add the blog name.
-	bloginfo( 'name' );
+        // Add the blog name.
+        bloginfo( 'name' );
 
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
+        // Add the blog description for the home/front page.
+        $site_description = get_bloginfo( 'description', 'display' );
+        if ( $site_description && ( is_home() || is_front_page() ) )
+            echo " | $site_description";
 
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'wedevs' ), max( $paged, $page ) );
+        // Add a page number if necessary:
+        if ( $paged >= 2 || $page >= 2 )
+            echo ' | ' . sprintf( __( 'Page %s', 'dokan' ), max( $paged, $page ) );
 
-	?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
+        ?></title>
 
-<?php wp_head(); ?>
-</head>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-<body <?php body_class(); ?>>
-    <div id="page" class="hfeed site">
-        <?php do_action( 'before' ); ?>
-        <header id="masthead" class="site-header" role="banner">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <hgroup>
-                            <h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?> <small> - <?php bloginfo( 'description' ); ?></small></a></h1>
-                        </hgroup>
-                    </div><!-- .span12 -->
-                </div><!-- .row -->
-            </div><!-- .container -->
+        <noscript>
+            <link rel="stylesheet" type="text/css" href="<?php print THEMEROOT ?>/assets/css/noscript.css" />
+        </noscript>
 
-            <div class="menu-container">
+        <?php wp_head(); ?>
+    </head>
+    <body class="woocommerce">
+        <header>
+            <!-- HEADER TOP CONTAINER -->
+            <div class="header-top-wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="left-header-top-content col-md-6">
+                            <div class="header-top-text">24/7 Customer Support (01) 123 456 YOUR STORE</div>
+                        </div>
+                    </div><!-- end .row -->
+                </div><!-- end .container -->
+            </div><!-- end .header-top-wrapper -->
+
+            <!-- HEADER MIDDLE WRAPPER -->
+            <div class="header-middle-wrapper">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-                            <nav role="navigation" class="site-navigation main-navigation clearfix">
-                                <h1 class="assistive-text"><i class="icon-reorder"></i> <?php _e( 'Menu', 'wedevs' ); ?></h1>
-                                <div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'wedevs' ); ?>"><?php _e( 'Skip to content', 'wedevs' ); ?></a></div>
-                                    <nav class="navbar navbar-default" role="navigation">
-                                        <div class="navbar-header">
-                                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                                                <span class="sr-only">Toggle navigation</span>
-                                                <span class="icon-bar"></span>
-                                                <span class="icon-bar"></span>
-                                                <span class="icon-bar"></span>
-                                            </button>
-                                            <a class="navbar-brand" href="<?php bloginfo('url')?>"><i class="icon-home"></i></a>
-                                        </div>
-                                        <div class="collapse navbar-collapse navbar-ex1-collapse">
-                                            <?php
-                                                wp_nav_menu( array(
-                                                    'menu'              => 'primary',
-                                                    'theme_location'    => 'primary',
-                                                    // 'depth'             => 3,
-                                                    'container'         => 'div',
-                                                    'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
-                                                    'menu_class'        => 'nav navbar-nav',
-                                                    'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                                                    'walker'            => new wp_bootstrap_navwalker())
-                                                );
-                                            ?>
-                                        </div>
-                                    </nav>
-                            </nav><!-- .site-navigation .main-navigation -->
-                        </div><!-- .span12 -->
-                    </div><!-- .row -->
-                </div><!-- .container -->
-            </div> <!-- .menu-container -->
-        </header><!-- #masthead .site-header -->
+                            <h1 class="logo heading-title"><a href="#"><img src="assets/images/logo.png" alt="Dokan"></a></h1>
 
-        <div id="main" class="site-main">
-            <div class="container content-wrap">
-                <div class="row">
+                            <div class="header-woo-content">
+                                <div class="header-bottom-wishlist selector">
+                                                                    
+                                    <div class="wc-tini-account-wrapper ">
+                                        <div class="wc-tini-account-control ">
+                                    
+                                            <a href="#" title="My Account" class="test"><span>Login / Register</span></a>    
+                                        </div>
+                                        
+                                        <div class="form_drop_down drop-down-container ">
+                                            <div class="form-wrapper">              
+                                                <div class="form-wrapper-body">
+                                                    <form name="loginform-custom" id="loginform-custom" action="#" method="post">
+                                                        <p class="login-username">
+                                                            <label for="user-login">Username</label>
+                                                            <input type="text" name="log" id="user-login" class="input" value="" size="20">
+                                                        </p>
+                                                        <p class="login-password">
+                                                            <label for="user_pass">Password</label>
+                                                            <input type="password" name="pwd" id="user-pass" class="input" value="" size="20">
+                                                        </p>
+                                                        <p class="login-submit">
+                                                            <input type="submit" name="wp-submit" id="wp-submit" class="button-primary" value="Login">
+                                                        </p>
+                                                    </form>
+                                                </div>
+
+                                                <div class="form_wrapper_footer">
+                                                    or New to Dokan?<span><a class="button" href="#">Register</a></span>
+                                                </div>
+                                            </div>  
+                                        </div>
+                                    </div>
+                                </div><!-- end .header-bottom-wishlist -->
+                                
+                                <div class="shopping-cart shopping-cart-wrapper selector">
+                                    <div class="wc-tini-cart-wrapper">
+                                        <ul class="cart-list list-unstyled">
+                                            <li>
+                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php printf( __( 'Cart %s', 'dokan' ), '<span class="dokan-cart-amount-top">(' . WC()->cart->get_cart_total() . ')</span>' ); ?> <b class="caret"></b></a>
+
+                                                <ul class="dropdown-menu">
+                                                    <li>
+                                                        <div class="widget_shopping_cart_content"></div>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div><!-- end .header-woo-content -->
+                            
+                            <div class="header-search">
+                                <form role="search" method="get" id="searchform" action="#">
+
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                                          All Categories
+                                          <span class="caret"></span>
+                                        </button>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#">Cameras &amp; Camcorders</a></li>
+                                            <li><a href="#">Computers &amp; Laptops</a></li>
+                                            <li><a href="#">Game &amp; Toys</a></li>
+                                            <li><a href="#">Gaming &amp; Consoles</a></li>
+                                            <li><a href="#">Home Entertainment</a></li>
+                                            <li><a href="#">Smartphones &amp; Tablets</a></li>
+                                            <li><a href="#">Televisions</a></li>
+                                        </ul>
+                                    </div>
+            
+                                     <div class="wc-search-form">
+                                         <label class="screen-reader-text" for="s">Search for:</label>
+                                         <input type="text" value="" name="s" id="s" placeholder="Search for products" />
+                                         <input type="submit" title="Search" id="searchsubmit" value="Search" />
+                                     </div>
+                                </form>
+                                 <?php // dynamic_sidebar( 'sidebar-header' ) ?>
+                            </div><!-- end .header-search -->
+                        </div><!-- end .col-md-12 -->
+                    </div><!-- end .header-middle-wrapper -->
+                </div><!-- end .row -->
+            </div><!-- end .container -->
+
+            <!-- HEADER BOTTOM -->
+            <div class="header-bottom-wrapper">
+                <div class="container">
+                    <div class="row">
+
+                        <!-- VERTICAL MENU -->
+                        <div class="vertical-menu col-md-3">
+                            <div class="cat-shop">Select Shop By Category  <span class="caret pull-right"></span> </div>
+                            
+                            <!-- Mega Menu -->
+                            <div class="mega-menu-wrapper">
+                                <aside id="#" class="widget dokan-category-menu">
+                                    <?php dokan_category_widget(); ?>
+                               </aside>
+
+                            </div><!-- end .mega-menu-wrapper -->
+                        </div><!-- end .vertical-menu -->
+
+
+
+                        
+                        
+                        <!-- MAIN MENU -->
+                        <div class="main-menu col-md-9">
+                            <?php
+                                wp_nav_menu( array(
+                                    'menu'              => 'primary',
+                                    'theme_location'    => 'top-left',
+                                    'depth'             => 0,
+                                    'container'         => 'div',
+                                    'container_class'   => 'collapse navbar-collapse navbar-top-collapse',
+                                    'menu_class'        => 'sf-menu',
+                                    'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                                    'walker'            => new wp_bootstrap_navwalker())
+                                );
+                            ?>
+                        </div><!-- end .main-menu -->
+
+                    </div><!-- end .row -->
+                </div><!-- end .container -->
+            </div><!-- end .header-bottom-wrapper -->
+
+        </header>
+
+
+        <div class="breadcrumb-wrapper container">
+            <?php do_action( 'dokan_child_after_header' ); ?>
+        </div><!-- end .breadcrumb-wrapper -->
